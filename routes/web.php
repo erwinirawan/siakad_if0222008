5c1c391129ed9952e3f3800b49af73cefc6febf3
Zamani <azacky302@gmail.com>
2024-07-03 15:24:37 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\FakultasController;
+use App\Http\Controllers\ProgramStudiController;
+use App\Models\ProgramStudi;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/program-studi', [ProgramStudiController::class, 'index'])->name('program-studi');
+Route::resource('/fakultas', FakultasController::class);
+Route::resource('/program-studi', ProgramStudiController::class);
